9ee00df09b1157cc3537e381c5c86fa756848244
jia-ayo <milkjia11@gmail.com>
2022-11-04 11:13:59 +0100

    minor

--- pages/index.js
+++ pages/index.js
@@ -39,8 +39,9 @@ export default function Home() {
         <div className="text-center p-10"> 
           <h2 className="text-5xl py-3 text-teal-600 font-medium md:text-6xl ">Ilori Joshua</h2>
           <h3 className="text-2xl py-2 md:text-3xl lg:text-4xl dark:text-slate-200">Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800 md:text-xl lg:text-2xl max-w-xl mx-auto dark:text-slate-100">I am a developer that works with web and blckchain technology. 
-             And would love to work to build/upgrade your idea
+          <p className="text-md py-5 leading-8 text-gray-800 md:text-xl lg:text-2xl max-w-xl mx-auto dark:text-slate-100">
+          I am a developer that works with web and blockchain technology. 
+          I would love to work to build/upgrade your idea and make your imagination a reality.
           </p>
         </div>
         <div className="text-4xl flex justify-center gap-10 py-2 text-gray-600 dark:text-slate-100">
@@ -57,19 +58,23 @@ export default function Home() {
         <div className="text-center">
           <h3 className="text-5xl py-1 dark:text-slate-100">Service i offer</h3>
           <p className="text-md py-5 leading-8 text-gray-800 dark:text-slate-100">
-            Since i started my development journey i have worked with a few project
+            Since i started my development journey I have worked with a few projects
             In the <span className="text-teal-200">Nft</span> space. 
-            I worked remotely for all the projects, I am looking for opportunity to work on bigger projects (grow) I love challenges
+            I worked remotely for all the projects, I am looking for opportunity to work on bigger projects (grow). I love challenges
           </p>
           <p className="text-md py-5 leading-8 text-gray-800 dark:text-slate-100">
-            I offer a wide range of service from web development to NFT development(full project), Dapp creation and much more skill set
+          Since I started my development journey I have worked on a few projects In the NFT space. 
+          I have worked remotely for all the projects, I am looking for opportunity to work on bigger projects (grow),
+           I love challenges.
+          I offer a wide range of service from web development to NFT development(full project), 
+          Dapp creation and other skillsets.
           </p>
         </div>
         <div className="md:flex gap-10">
           <div className="shadow-lg text-center rounded-xl p-10 my-10 dark:bg-slate-100">
              <Image className="mx-auto" src={nftImg} width="100" height="100"/>
-             <h3 className="text-lg font-medium pt-8 pb-2">SmarContract development</h3>
-             <h4 className="py-4 text-teal-600 ">The tools I use</h4>
+             <h3 className="text-lg font-medium pt-8 pb-2">SmartContract development</h3>
+             <h4 className="py-4 text-teal-600 ">The Tools I Use</h4>
              <p className="text-gray-800 py-1">Rust</p>
              <p className="text-gray-800 py-1">Metaplex</p>
              <p className="text-gray-800 py-1">solidity</p>
@@ -78,7 +83,7 @@ export default function Home() {
           <div className="shadow-lg text-center rounded-xl p-10 my-10 dark:bg-slate-100">
              <Image className="mx-auto mt-10 mb-2" src={frontImg} width="100" height="100"/>
              <h3 className="text-lg font-medium pt-8 pb-2">Frontend development</h3>
-             <h4 className="py-4 text-teal-600 ">The tools I use</h4>
+             <h4 className="py-4 text-teal-600 ">The Tools I Use</h4>
              <p className="text-gray-800 py-1">Next.js/Reactjs</p>
              <p className="text-gray-800 py-1">tailwind/bootstrap</p>
              <p className="text-gray-800 py-1">css/html/javascript</p>
@@ -86,7 +91,7 @@ export default function Home() {
           <div className="shadow-lg text-center rounded-xl p-10 my-10 dark:bg-slate-100">
              <Image className="mx-auto" src={backImg} width="100" height="100"/>
              <h3 className="text-lg font-medium pt-8 pb-2">Backend development</h3>
-             <h4 className="py-4 text-teal-600 ">The tools I use </h4>
+             <h4 className="py-4 text-teal-600 ">The Tools I Use </h4>
              <p className="text-gray-800 py-1">Node.js</p>
              <p className="text-gray-800 py-1">Express</p>
              <p className="text-gray-800 py-1">mongodb</p>
@@ -96,12 +101,12 @@ export default function Home() {
         
       </section>
       <section>
-        <div>
+        <div className="pb-2">
           <div className="text-center">
             <h3 className="text-3xl py-1 md:text-4xl lg:text-5xl dark:text-slate-100">Portfolio</h3>
           </div>
           <div className="md:flex gap-10">
-          <div className="shadow-lg text-center rounded-xl p-10 my-10 dark:bg-slate-100">
+          <div className="shadow-lg text-center rounded-xl p-10 my-10 dark:bg-slate-100 dark:shadow-slate-600/40">
              <Image className="mx-auto mt-10 mb-2" src={raskyImg} width="100" height="100"/>
              <h3 className="text-lg font-medium pt-8 pb-2">Fullstack development</h3>
              <h4 className="py-4 text-teal-600 ">Rasky Rebels</h4>
